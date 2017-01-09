fa93b9c957a441faeb826b539a53ff31410e99af
Linda Zhou <lz28330@gmail.com>
2017-01-09 21:50:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var animal = 'dog'
 
 function myAnimal() {
-  return animal
+  return myAnimal() = animal
 }
 
-function yourAnimal() {
+function yourAnimal(string) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return string.animal()
 }
 
 function add2(n) {
