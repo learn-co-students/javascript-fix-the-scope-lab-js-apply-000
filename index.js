6b3732e28a2d44fae0b669bc02de40493a561e43
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-03-26 02:19:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+function myAnimal (animal){var animal= 'dog'
   return animal
 }
 
-function add2(n) {
-  return n + two
+function yourAnimal() {var animal='cat'
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
 
-  // Feel free to move things around!
-  const two = 2
+var animal='cat'
+  return animal
 }
+function add2(n) {
+  const two = 2;
+  return n + two}
 
 var funkyFunction = function() {
   return function() {
