bb768aa10a5a5ef7c95dd2e7e815635f6c3c2c04
Eyob Kinfe <eyobkk@yahoo.com>
2017-04-10 19:59:13 +0000

    Done.

--- index.js
+++ index.js
@@ -26,4 +26,26 @@ var funkyFunction = function() {
 
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // NOTE: To pass this final test, you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+function add2(n) {
+const two = 2
+return 'n + two'
+}
+function yourAnimal() {
+return animal= "cat"
+return animal
+}
+const two = 2;
+function add2(n) {
+  return n + two;
+}
+var funkyFunction = function(){
+return function(){
+return "FUNKY!"
+}
+}
+var theFunk = funkyFunction()();
