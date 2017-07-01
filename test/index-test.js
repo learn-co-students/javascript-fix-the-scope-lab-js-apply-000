466cd0b219e25dc5e3f85f399e4ebc2e1123b121
Silvia Chung <schung97@gmail.com>
2017-07-01 22:09:04 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,27 @@
-/*global describe, it */
+/*global scope: variables declared outside of function */
+//-------------------------------------------------
+
+function myAnimal() {
+  var animal = 'dog';
+  return animal;
+}
+myAnimal();
 
 describe('Fix the Scope', function() {
-  describe('myAnimal()', function() {
-    it('returns my animal', () => {
-      expect(window.myAnimal()).toEqual('dog')
-    })
+})
+
+describe('myAnimal()', function() {
+  it('returns my animal', () => {
+    expect(window.myAnimal()).toEqual('dog')
   })
+})
+//----------------------------------------------------
+function yourAnimal() {
+  var animal = 'cat';
+  return animal;
+}
+
+yourAnimal();
 
   describe('yourAnimal()', function() {
     it('returns your animal', function(){
@@ -16,24 +32,33 @@ describe('Fix the Scope', function() {
       expect(window.yourAnimal.toString()).toNotContain("return 'cat'")
     })
   })
-
+//--------------------------------------------------------
+function add2(n) {
+  var two = 2;
+  return n + two;
+}
   describe('add2(n)', function() {
     it('adds two to n', function() {
       const n = Math.floor(Math.random() * 1000)
       expect(window.add2(n)).toEqual(n + 2)
     })
   })
-
+//-----------------------------------------------
+var funkyFunction = function() {
+  return function() {
+    return 'FUNKY!';
+  }
+}
   describe('funkyFunction()', function() {
     it('returns a function', function() {
       expect(typeof window.funkyFunction()).toEqual('function')
     })
   })
 
+var theFunk = funkyFunction()();
+
   describe('theFunk', function() {
     it('is "FUNKY!"', function() {
       expect(window.theFunk).toEqual('FUNKY!')
     })
   })
-
-})
