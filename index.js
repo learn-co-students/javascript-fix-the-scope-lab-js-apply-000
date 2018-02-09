143316133591132e0ad51310a55502703bccdd28
Sophia Song <sophia.song@tufts.edu>
2018-02-09 19:53:45 +0000

    Done.

--- index.js
+++ index.js
@@ -11,19 +11,21 @@ function yourAnimal() {
   return animal
 }
 
-function add2(n) {
-  return n + two
+function add2(n) 
+ { return n + two}
 
   // Feel free to move things around!
   const two = 2
-}
 
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
-}
+
+var funkyFunction = funkyFunction();
+{ return "funkyFunction";}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = "FUNKY!"; ()
+var insideTheFunkyFunction= "funkyFunction"; ()
+{ return "insideTheFunkFunction" ;}
+
+
+
