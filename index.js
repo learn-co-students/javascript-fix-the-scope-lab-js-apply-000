8623e4440fcb0eb639e7f087c1a7591de08f0724
Asher Ferri <asherferri85@gmail.com>
2018-03-15 03:23:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var animal = 'dog'
+var animal = 'dog';
 
 function myAnimal() {
-  return animal
+  return animal;
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return animal;
 }
 //we add this next to override past
 function yourAnimal() {
@@ -26,11 +26,11 @@ function add2(n) {
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
+    return "FUNKY!";
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction;
 var theFunk = funkyFunction()()
