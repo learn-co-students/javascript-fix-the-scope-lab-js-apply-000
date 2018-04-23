68bf30853592310d84c6074c8bbf1743881214c1
Sam <sammyliebman@gmail.com>
2018-04-23 16:06:45 +0000

    Done.

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  //define animal variable as 'cat' so that myAnimal function can return the right animal
+  var animal = 'cat'
   return animal
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
+  /*flipped the two equations around because once return 
+  statement runs the function ends rendering anything 
+  afterwards irrelevant*/
   const two = 2
+  return n + two
+  
 }
 
 var funkyFunction = function() {
@@ -26,4 +27,7 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+
+//the variable funkyFunction returns only the function, not the output of the function. So, in order to return "FUNKY!", i need to call the function twice so use ()()
+var theFunk = funkyFunction()()
+
