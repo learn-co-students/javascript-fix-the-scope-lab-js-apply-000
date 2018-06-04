dbb84a0cf02ef7fb72928ef672af1cf841cad13d
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-04 23:44:04 +0000

    Done.

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + 2
+  const two = 2
+  return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() 
