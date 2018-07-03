895a9aa3bb15eadbf5d66bdd1c9afb30233f66cf
Landen Danyluk <landendanyluk@gmail.com>
2018-07-03 17:10:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Strings Lab
+
 var animal = 'dog'
 
 function myAnimal() {
@@ -5,17 +7,13 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  animal = "cat";
   return animal
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
@@ -24,6 +22,4 @@ var funkyFunction = function() {
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()();
