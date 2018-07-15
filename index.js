fac3b5455fac26e61efbd041cb099d3c69d1e361
Tiffany Choong <t.choong@yahoo.com>
2018-07-15 18:50:50 +0000

    Done.

--- index.js
+++ index.js
@@ -29,3 +29,5 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()()
+
+//Added the 1st parenthesis to execute the outside function a.k.a funkyFunction. Added the 2nd parenthesis next to "funkyFunction()" since it's essentially the name of the inside function that we want to execute.
