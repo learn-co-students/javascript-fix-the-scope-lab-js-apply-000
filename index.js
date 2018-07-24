7ca9994a5125ca053ab17b94eb68e69fe89e31a3
nouran.nadhem@gmail.com Nadhem Haider <nouran.nadhem@gmail.com>
2018-07-24 09:18:17 +0000

    Done.

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ function myAnimal() {
   return animal
 }
 
-
 function yourAnimal() {
   var animal = 'cat'
   return animal
 }
-
 
 function add2(n) {
   const two = 2
   return n + two
 }
 
-
-function funkyFunction () {
+var funkyFunction = function () {
   return function (){
     return "FUNKY!"
   }
