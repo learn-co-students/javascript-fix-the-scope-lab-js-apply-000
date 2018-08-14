87a278a5456566421884dbfe2330de8297afd564
mjgreene2010@gmail.com <mjgreene2010@gmail.com>
2018-08-14 18:00:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 var animal = 'dog'
 
-function myAnimal() {
+function myAnimal(string) {
   return animal
 }
 
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+function yourAnimal(string) {
+  var animal = 'cat'
   return animal
 }
 
+ 
+  const two = 2
+ 
 function add2(n) {
   return n + two
-
+  
+ 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
+var theFunk = "FUNKY!"
+
+function funkShell() {
+    return theFunk
 }
+
+function funkyFunction() {
+  return funkShell()
+}
+
+
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+// NOTE: you only need to modify the code below this line
+
+
+
+
+
+  
