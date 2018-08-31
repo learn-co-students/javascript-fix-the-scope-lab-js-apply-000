8c7658b426619875026f7d8529df8f49fbb50860
gianna.jolibeth15@gmail.com <gianna.jolibeth15@gmail.com>
2018-08-31 00:08:48 +0000

    Done.

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ var animal = 'dog'
 
 function myAnimal() {
   return animal
+  
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  var animal = "cat" // local variable overrides the global variable
   return animal
 }
-
-function add2(n) {
+const two = 2 // Looking at the index-test, the constant was supposed to be in the "global scope" which is anything outside a function 
+function add2(n) { 
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
@@ -26,4 +24,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()() //the first set of parenthesis invokes the first function and then the second set of parenthesis invokes the second function (diggin)--- setting the variable "theFunk" to the RETURN value of the funkyFunction -- step by step no jumping
+
