d074739a6faafe259063aaceb59db0adf3cb5613
Berkay Akcakil <burkeakcakil@gmail.com>
2018-11-19 05:10:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var animal = 'dog'
-
+//Animal is already defined, so we just return it//
 function myAnimal() {
   return animal
 }
-
+//We re-assign our variable in order to return the new value 'cat'//
 function yourAnimal() {
-  var animal = 'cat'
+  var animal = 'cat';
   return animal
 }
-
+/*Return always comes first and ignores what comes after. Therefore; we move
+  our constant above the return*/
 function add2(n) {
-  const two = 2
+  const two = 2;
   return n + two
 }
 
@@ -19,5 +20,5 @@ var funkyFunction = function() {
     return "FUNKY!"
   }
 }
-
+//By adding another "()", we tell function to execute the string within//
 var theFunk = funkyFunction()()
