89dbeba8515f16b1560d574e11808ce8dbaddfc7
Rufus <ruufman@gmail.com>
2019-01-13 16:12:15 +0000

    Done.

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function add2(n) {
   const two = 2
 }
 
-var funkyFunction = function() {
+//var funkyFunction = function() {
+function funkyFunction () {
   return function() {
     return "FUNKY!"
   }
@@ -27,3 +28,29 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction
+var theFunk = funkyFunction()
+
+function yourAnimal () {
+  var animal = 'cat';
+  return animal;
+}
+
+//describe('add2(n)', function() {
+  //it('adds two to n', function() {
+    //const n = Math.floor(Math.random() * 1000)
+    //expect(window.add2(n)).toEqual(n + 2)
+  //})
+//})
+
+function add2 (n) {
+  var two = 2;
+   return n + two;
+}
+
+var funkyFunction = function() {
+//function funkyFunction () {
+  return function() {
+    return 'FUNKY!'
+  }
+}
+var theFunk = funkyFunction()()
