23ca6b3ab50a0b962753e9fc58833a42dfee5e44
Adrian Samaniego <llathander@gmail.com>
2019-01-13 00:58:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var animal = 'dog'
+const two = 2;
 
 function myAnimal() {
+  // let animal = 'cat';
   return animal
 }
 
@@ -8,22 +10,28 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  let animal = "cat";
   return animal
 }
 
 function add2(n) {
-  return n + two
+  return n + two;
+ 
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
+    return "FUNKY!";
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = "FUNKY!"
+
+
+
+// i tried to use the absolute value method to solve the math part :).....such craziness, when the solution was so simple!le
