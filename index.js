447a5c0f46ea60429e8cfae3913408387bdef5ba
jayson jeckley <jeckleyson00@gmail.com>
2019-01-25 03:29:20 +0000

    Done.

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function yourAnimal() {
   return animal
 }
 
-function add2(n) {
+function add2(n) { 
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
