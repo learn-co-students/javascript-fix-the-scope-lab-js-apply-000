abd191edba99db10961ee9be47ba64c83ee7080d
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-02-19 15:31:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var animal = 'dog'
 
 function myAnimal() {
+  
   return animal
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+ return animal = "cat";
   return animal
 }
 
 function add2(n) {
+ const two = 2;
   return n + two
-
-  // Feel free to move things around!
-  const two = 2
 }
+  // Feel free to move things around!
+  https://previews.123rf.com/images/lamika/lamika1507/lamika150700002/42441517-white-and-gold-pattern-abstract-geometric-modern-background-vector-illustration-shiny-backdrop-textu.jpg
+
 
 var funkyFunction = function() {
   return function() {
@@ -27,3 +27,4 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction
+var theFunk = funkyFunction()()
