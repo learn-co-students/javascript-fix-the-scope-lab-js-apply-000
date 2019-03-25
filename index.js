a92d5ed16817ceeb2269ab94a7c806da79c51f51
Eric Wang <eric.l.wang@blackrock.com>
2019-03-25 00:30:11 +0000

    Done.

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-//funkyFunction().function()
 var test = funkyFunction()
 var theFunk = test()
 theFunk()
