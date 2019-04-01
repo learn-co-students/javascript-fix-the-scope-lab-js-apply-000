fd3a808496d7a13010e6ee47d1a827828f9f3a84
Luke Menard <lukemenard@gmail.com>
2019-04-01 18:54:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,52 +1,58 @@
 var animal = 'dog';
-//Defines the global variable 'animal'
-//Assigns 'animal' the string 'dog'
+// 1. Defines the global variable 'animal'
+      // Assigns 'animal' the string 'dog'
 
 function myAnimal() {
   return animal;
 }
-//Defines the function 'myAnimal' that accepts no parameters
-//'myAnimal' returns the global variable 'animal'
-//Because 'myAnimal' is in the same scope as 'animal,' the function will return the string 'dog' here
+// 1. Defines the function 'myAnimal'
+      // 'myAnimal' accepts no parameters
+// 2. Returns the global variable 'animal'
+      // 'myAnimal' is in the same scope as 'animal'
+      // The function will return the string 'dog' here
 
-function yourAnimal(animal) {
-  var animal = 'cat'
+function yourAnimal() {
+  var animal = 'cat';
   return animal;
 }
-//Defines the function 'yourAnimal' that accepts one parameter, 'animal'
-//Defines the LOCAL variable 'animal'
-//Assigns 'animal' the string 'cat' WITHIN the 'yourAnimal' function
-//'yourAnimal' returns the LOCAL variable 'animal'
-//Because 'animal' is a local variable within the same scope as the 'yourAnimal' return command, the function will return the string 'cat' here
+// 1. Defines the function 'yourAnimal'
+      //'yourAnimal' accepts no parameters
+// 2. Defines the LOCAL variable 'animal'
+      // Assigns 'animal' the string 'cat' WITHIN the 'yourAnimal' function
+// 3. Returns the LOCAL variable 'animal'
+      // 'animal' is a local variable within the same scope as the 'yourAnimal' return command
+      // The local variable 'animal' overrides the global variable 'animal' within 'yourAnimal'
+      // The function will return the string 'cat' here
 
 function add2(n) {
   const two = 2;
   return n + two;
 }
-//Defines the function 'add2' that accepts one parameter, 'n'
-//Defines the local constant variable 'two'
-//Assigns 'two' the integer 2
-//'add2' returns 2 plus the entered integer
+// 1. Defines the function 'add2'
+      // 'add2' accepts one parameter, 'n'
+// 2. Defines the local constant variable 'two'
+      // Assigns 'two' the integer 2
+// 3. Returns the entered integer 'n' plus 2
 
 var funkyFunction = function() {
   return function() {
     return "FUNKY!";
   };
 };
-//Defines the global variable 'funkyFunction'
-//Assigns funkyFunction the execution of a function that accepts no parameters
-//The first function returns the execution of a second function that accepts no parameters
-//The second function returns the string "FUNKY!"
+// 1. Defines the global variable 'funkyFunction'
+      // Assigns funkyFunction the execution of a function that accepts no parameters
+// 2. The first function returns the execution of a second function that accepts no parameters
+// 3. The second function returns the string "FUNKY!"
 
 var theFunk = funkyFunction();
-//Defines the global variable 'theFunk'
-//Assigns 'theFunk' the execution of the global variable 'Funky Function'
+// 1. Defines the global variable 'theFunk'
+      //Assigns 'theFunk' the execution of the global variable 'funkyFunction'
 
 theFunk = theFunk();
-//Assigns 'theFunk' the execution of itself
+// 1. Assigns 'theFunk' the execution of itself
 
-//1. 'theFunk' executes 'theFunk'
-//2. 'theFunk' executes 'funkyFunction'
-//3. 'funkyFunction' executes a function
-//4. This function returns a function
-//5. The second function returns the string "FUNKY!"
+// 1. 'theFunk' executes 'theFunk()'
+// 2. 'theFunk()' executes 'funkyFunction()'
+// 3. 'funkyFunction' executes a 'function()'
+// 4. This function returns a 'function()'
+// 5. The second function returns the string "FUNKY!"
