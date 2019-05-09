96723badd62ca4fb28646202202e3b0821c77926
captivating-form-0787 <avertrees447@gmail.com>
2019-05-09 19:31:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var animal = 'dog'
+var cat = 'cat'
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
+var theFunk = funkyFunction
 
 function myAnimal() {
   return animal
@@ -8,22 +12,18 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return cat
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+  return n + 2
 }
 
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
+var theFunk = funkyFunction()()
+
+function funkyFunction() {
+  return function theFunk(){
+    return 'FUNKY!'
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
