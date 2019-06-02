ade66f9a6942398d60a71e6c0a7487860adc3649
John Odom <jodom91@gmail.com>
2019-06-02 21:47:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-var myanimal = 'dog';
-var youranimal = 'cat';
-
 function myAnimal() {
-  return myanimal
+  var animal = 'dog';
+  return animal
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return youranimal
+  // Had to change the scope of animal variable to local from global
+  var animal = 'cat';
+  return animal
 }
 
 function add2(n) {
-   const two = 2;
+  const two = 2;
   return n + two;
   // Feel free to move things around!
 }
+
 
 var funkyFunction = function() {
   return function() {
@@ -26,4 +24,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = "FUNKY!"
+var theFunk = funkyFunction()()
