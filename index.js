c381d656b5f970db5ab7cad88d591aab983efe02
Sam Hoidal <sjhoidal@gmail.com>
2019-10-25 13:23:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-//changed declaration to array and referenced correct index
-var animal = ['dog', 'cat']
+
+var animal = "dog"
 
 function myAnimal() {
-  return animal[0]
+  return animal
 }
 
+//wrote var animal inside yourAnimal to shadow outter declaration
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal[1]
+  var animal = "cat"
+  return animal
 }
 
 //moved declaration above return statement within function 
@@ -26,5 +28,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = "FUNKY!"
+var theFunk = funkyFunction()()
   
