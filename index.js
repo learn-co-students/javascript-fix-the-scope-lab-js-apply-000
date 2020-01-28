f42a52f2b3196bee622006c0bdb6953c7d44769e
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-01-28 04:59:22 +0000

    Done.

--- index.js
+++ index.js
@@ -37,5 +37,4 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 // var theFunk = funkyFunction
 
-var theFunk = funkyFunction()
-theFunk()
+var theFunk = funkyFunction()()
