30660538936c8f336f19f21e48fc8d0df5558af3
essential-form-0157 <vasmith430@gmail.com>
2020-07-04 02:47:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var animal = 'dog'
 
+
 function myAnimal() {
   // You should not need to modify this function
   return animal
@@ -11,14 +12,15 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
+  var animal = "cat";
   return animal
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
@@ -29,4 +31,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction () ()
